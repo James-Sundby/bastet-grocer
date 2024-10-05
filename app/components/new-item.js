08845eb49925e01935f51eae3312587a778ba675
James <119804969+James-Sundby@users.noreply.github.com>
2024-10-05 11:22:21 -0600

    Reverts add item modal

--- app/components/new-item.js
+++ app/components/new-item.js
@@ -22,11 +22,6 @@ export default function NewItem({ onAddItem }) {
     setQuantity(1);
   };
 
-  const closeModal = () => {
-    resetForm();
-    document.getElementById("Add_Item_Dialog").close();
-  };
-
   const handleNameChange = (event) => {
     setName(event.target.value);
   };
@@ -40,63 +35,53 @@ export default function NewItem({ onAddItem }) {
   };
 
   return (
-    <>
-      <div className="w-full px-4 pb-2">
-        <button className="w-full btn btn-primary" onClick={() => document.getElementById('Add_Item_Dialog').showModal()}>Add Items</button>
-      </div>
-      <dialog id="Add_Item_Dialog" className="modal">
-        <div className="modal-box card-compact">
-          <form className="card-body" onSubmit={handleSubmit}>
-            <input
-              placeholder="Item Name"
-              type="text"
-              required
-              onChange={handleNameChange}
-              value={name}
-              className="input input-bordered text-base"
-            />
-            <div className="flex space-x-2">
-              <input
-                type="number"
-                min="1"
-                max="99"
-                required
-                onChange={handleQuantityChange}
-                value={quantity}
-                className="input input-bordered w-1/4 text-base"
-              />
-              <select
-                required
-                onChange={handleCategoryChange}
-                value={category}
-                className="select select-bordered flex-grow text-base"
-              >
-                <option disabled>Select a Category</option>
-                <option value="bakery">Bakery</option>
-                <option value="beverages">Beverages</option>
-                <option value="canned goods">Canned Goods</option>
-                <option value="dairy">Dairy</option>
-                <option value="deli">Deli</option>
-                <option value="dry goods">Dry Goods</option>
-                <option value="frozen foods">Frozen Foods</option>
-                <option value="household">Household</option>
-                <option value="meat">Meat</option>
-                <option value="produce">Produce</option>
-                <option value="snacks">Snacks</option>
-                <option value="other">Other</option>
-              </select>
-            </div>
-            <div className="flex gap-2">
-              <button type="submit" className="btn btn-primary flex-1">
-                Save
-              </button>
-              <button type="button" className="btn btn-error flex-1" onClick={closeModal}>
-                Close
-              </button>
-            </div>
-          </form>
+    <div className="card card-compact bg-base-100">
+      <form className="card-body" onSubmit={handleSubmit}>
+        <input
+          placeholder="Item Name"
+          type="text"
+          required
+          onChange={handleNameChange}
+          value={name}
+          className="input input-bordered text-base"
+        />
+        <div className="flex space-x-2">
+          <input
+            type="number"
+            min="1"
+            max="99"
+            required
+            onChange={handleQuantityChange}
+            value={quantity}
+            className="input input-bordered w-1/4 text-base"
+          />
+          <select
+            required
+            onChange={handleCategoryChange}
+            value={category}
+            className="select select-bordered flex-grow text-base"
+          >
+            <option disabled>Select a Category</option>
+            <option value="bakery">Bakery</option>
+            <option value="beverages">Beverages</option>
+            <option value="canned goods">Canned Goods</option>
+            <option value="dairy">Dairy</option>
+            <option value="deli">Deli</option>
+            <option value="dry goods">Dry Goods</option>
+            <option value="frozen foods">Frozen Foods</option>
+            <option value="household">Household</option>
+            <option value="meat">Meat</option>
+            <option value="produce">Produce</option>
+            <option value="snacks">Snacks</option>
+            <option value="other">Other</option>
+          </select>
         </div>
-      </dialog>
-    </>
+        <div className="flex">
+          <button type="submit" className="btn btn-primary flex-1">
+            Add Item to List
+          </button>
+        </div>
+      </form>
+    </div >
   );
 }
