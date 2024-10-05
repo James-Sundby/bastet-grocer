a8d1c485eed24ba53517607622dadb1c744c00cb
James <119804969+James-Sundby@users.noreply.github.com>
2024-10-05 13:14:12 -0600

    Adds new background to home page

--- app/page.js
+++ app/page.js
@@ -24,56 +24,68 @@ export default function Home() {
   }
 
   return (
-    <main className="flex flex-1 flex-col items-center justify-center">
+    <main className="flex flex-1 flex-col items-center justify-center ">
       {user ? (
         <>
-          <div className="card bg-base-100 -translate-y-16">
-            <figure>
-              <div className="avatar">
-                <div className="w-24 mask mask-squircle m-2">
-                  <Image
-                    src={user.photoURL}
-                    alt="user avatar"
-                    width={100}
-                    height={100}
-                  />
-                </div>
-              </div>
-            </figure>
-            <div className="card-body">
+          <div
+            className="hero h-custom"
+            style={{
+              backgroundImage: "url(/grocery.webp)",
+            }}
+          >
+            <div className="hero-overlay bg-opacity-75 bg-black"></div>
+            <div className="card bg-base-100 -translate-y-16 ">
+              <div className="card-body">
+                <figure>
+                  <div className="avatar">
+                    <div className="w-24 mask mask-squircle m-2">
+                      <Image
+                        src={user.photoURL}
+                        alt="user avatar"
+                        width={100}
+                        height={100}
+                      />
+                    </div>
+                  </div>
+                </figure>
+                <h2 className="card-title text-2xl justify-center mb-4">
+                  Welcome, {user.displayName ? user.displayName : user.email ? user.email : "User"}
+                </h2>
 
-              <h2 className="card-title text-2xl justify-center">
-                Welcome, {user.displayName ? user.displayName : user.email ? user.email : "User"}
-              </h2>
-
-              <div className="card-actions justify-center">
-                <div className="flex flex-col gap-4">
-                  <Link
-                    className="btn btn-primary btn-wide"
-                    href="/quick-add"
-                    aria-label="Continue to my shopping list"
-                  >
-                    Edit Quick Adds
-                  </Link>
-                  <Link
-                    className="btn btn-primary btn-wide"
-                    href="/shopping-list"
-                    aria-label="Continue to my shopping list"
-                  >
-                    Shopping List
-                  </Link>
-                  <button
-                    aria-label="Sign Out"
-                    onClick={handleSignOut}
-                    className="btn btn-primary btn-wide"
-                  >
-                    Sign Out
-                  </button>
+                <div className="card-actions justify-center">
+                  <div className="flex flex-col gap-4">
+                    <Link
+                      className="btn btn-primary btn-wide"
+                      href="/quick-add"
+                      aria-label="Continue to my shopping list"
+                    >
+                      Edit Quick Adds
+                    </Link>
+                    <Link
+                      className="btn btn-primary btn-wide"
+                      href="/shopping-list"
+                      aria-label="Continue to my shopping list"
+                    >
+                      Shopping List
+                    </Link>
+                    <button
+                      aria-label="Sign Out"
+                      onClick={handleSignOut}
+                      className="btn btn-primary btn-wide"
+                    >
+                      Sign Out
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
+            <div className="text-center text-gray-400 mt-auto pb-2">
+              <p>
+                Photo by <a href="https://unsplash.com/@scottiewarman?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" className="underline">Scott Warman</a> on
+                <a href="https://unsplash.com/photos/row-of-vegetables-placed-on-multilayered-display-fridge-NpNvI4ilT4A?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" className="underline"> Unsplash</a>
+              </p>
+            </div>
           </div>
-
         </>
       ) : (
         <>
