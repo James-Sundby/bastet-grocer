9c8ef6e4c42a7a45c6dcdefc229bb1c2aa9f4360
James <119804969+James-Sundby@users.noreply.github.com>
2024-11-28 18:01:46 -0700

    Adds next sharp for image optimization

--- app/page.js
+++ app/page.js
@@ -8,9 +8,8 @@ export default function Home() {
   return (
     <main className="flex flex-1 flex-col items-center justify-center" role="main">
 
-      <div id="server-placeholder" className="relative h-custom w-full">
-        <div className="absolute inset-0 bg-black/75"></div>
-        <div className="hero-content text-white text-center flex-col absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <div id="server-placeholder" className="relative h-custom w-full bg-zinc-900">
+        <div className="hero-content text-neutral-content text-center flex-col absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <div className="max-w-md">
             <p>Loading...</p>
           </div>
